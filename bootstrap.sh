ccea1926a24392e0bb192f8a2caec447449bd904
Faouzi <57812398+faouziMohamed@users.noreply.github.com>
2021-01-08 05:12:47 +0100

    Configuring install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-
 cd "$(dirname "${BASH_SOURCE}")";
 git pull origin main;
 
-function install_() {
-    rsync --exclude-from=.rsync_ignore -avhl --no-perms . ${1};
+function install_copy() {
+    rsync --exclude-from=.rsync_ignore -avhl --no-perms home/user/ ~;
     source ~/.bash_profile;
+    exec $SHELL;
 }
 
 function main(){
 	if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	install_ test;
+	install_copy;
 	else
 		read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 		echo "";
 		if [[ $REPLY =~ ^[Yy]$ ]]; then
-			install_ test;
+			install_copy;
 		fi;
-fi;
+    fi;
 }
 
 
 main $@;
-unset install_;
+unset install_copy;
