9bb04e250d7150ce5861d2b5941703b714633e45
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-01-09 02:49:44 +0100

    Update script

--- gitaliases.sh
+++ gitaliases.sh
@@ -22,15 +22,14 @@ ls,ls-files")
 
 # set the separator to new line
 IFS_BKP="${IFS}"
-
 IFS="
 "
 
 for var in ${GIT_ALIASES[*]}; do
-    __command=$(echo ${var} | cut -f1 -d,)
+    __command=$(echo "${var}" | cut -f1 -d,)
 
-    # get the alias and replace escape double quotes by simple quotes
-    __alias=$(echo ${var} | cut -f2 -d, | sed "s-\"-'-g")
+    # get the alias and replace double quotes by simple quotes (sep='-')
+    __alias=$(echo "${var}" | cut -f2 -d, | sed "s#\"#'#g")
 
     # TODO: if verbose the print each command
     echo -en "${ITALIC}${LIGHT_RED}git ${RESET}"
@@ -39,9 +38,11 @@ for var in ${GIT_ALIASES[*]}; do
     echo -en "${ITALIC}${SOFT_GRAY}alias${RESET}"
     echo -en "${WHITE}.${RESET}"
     echo -en "${ITALIC}${SOFT_GRAY}${__command} ${RESET}"
-    echo -e "\"${ITALIC}${ORANGE}${__alias}${RESET}\""
+    echo -e  "\"${ITALIC}${ORANGE}${__alias}${RESET}\""
 
     # TODO:Create command here
-    git config --global alias."${__command}" "${__alias}"
+    # git config --global alias."${__alias}" "${__command}"
 done
+
+# Restore default separator
 IFS="${IFS_BKP}"
