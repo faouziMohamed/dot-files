1322703a853a203a9b3d62a297589b98f88dd323
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-01-10 04:36:04 +0100

    Update git prompt for the count of stashed files

--- gitaliases.sh
+++ gitaliases.sh
@@ -6,19 +6,34 @@
     [ -f "home/user/.color" ] &&
     source "home/user/.color"
 
-GIT_ALIASES=("cancel,reset --hard HEAD^
+verbose=''
+if [[ $# -gt 0 ]]; then
+    case $1 in
+        "-v" | "--verbose") verbose=true ;;
+        -* | *)
+
+            cat << EOF
+            USAGE $(basename)
+EOF
+            ;;
+    esac
+fi
+GIT_ALIASES=("
+cancel,reset --hard HEAD^
 unstage,reset HEAD
 graph,log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ci) %C(bold blue)<%an>%Creset'
 ckt,checkout
-st,status
+stt,status
 sh,stash
 a,add
 cm,commit
 cma,commit --amend
 cmm,commit -m
+cmam,commit --amend -m
 pl,pull
 ps,push
-ls,ls-files")
+ls,ls-files
+sst,status -s #stand for short status")
 
 # set the separator to new line
 IFS_BKP="${IFS}"
@@ -26,20 +41,20 @@ IFS="
 "
 
 for var in ${GIT_ALIASES[*]}; do
+    if [  -n "$var" ]; then echo "Vide $var"; fi
     __command=$(echo "${var}" | cut -f1 -d,)
-
-    # get the alias and replace double quotes by simple quotes (sep='-')
+    # get the alias and replace double quotes by simple quotes (sep='#')
     __alias=$(echo "${var}" | cut -f2 -d, | sed "s#\"#'#g")
-
-    # TODO: if verbose the print each command
-    echo -en "${ITALIC}${LIGHT_RED}git ${RESET}"
-    echo -en "${ITALIC}config ${RESET}"
-    echo -en "${ITALIC}${SALMON}--global ${RESET}"
-    echo -en "${ITALIC}${SOFT_GRAY}alias${RESET}"
-    echo -en "${WHITE}.${RESET}"
-    echo -en "${ITALIC}${SOFT_GRAY}${__command} ${RESET}"
-    echo -e  "\"${ITALIC}${ORANGE}${__alias}${RESET}\""
-
+    if [[ ${verbose} ]]; then
+        # TODO: if verbose the print each command
+        echo -en "${ITALIC}${LIGHT_RED}git ${RESET}"
+        echo -en "${ITALIC}config ${RESET}"
+        echo -en "${ITALIC}${SALMON}--global ${RESET}"
+        echo -en "${ITALIC}${SOFT_GRAY}alias${RESET}"
+        echo -en "${WHITE}.${RESET}"
+        echo -en "${ITALIC}${SOFT_GRAY}${__command} ${RESET}"
+        echo -e "\"${ITALIC}${ORANGE}${__alias}${RESET}\""
+    fi
     # TODO:Create command here
     # git config --global alias."${__alias}" "${__command}"
 done
