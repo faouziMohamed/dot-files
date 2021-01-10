8a1bfca82f7b5e9f8b88fe2f8984b2abb1d24c9a
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-01-10 09:49:58 +0100

    Improvement of install script, generating gitconfig file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,54 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")" || return 1
+
+verbose='--verbose'
+prettify=''
+force=''
+
+if [[ $# -gt 0 ]]; then
+  while [ $1 ]; do
+    case $1 in
+      -*)
+        case $1 in # -n depends in -q to be used
+          -q | --quiet) verbose='--quiet' ;;
+          -n | --no-prettify) prettify='--no-prettify' ;;
+          -f | --force) force='--force' ;;
+          *)
+            echo -e "${BOLD}USAGE $(basename "$0")${RESET} : $(basename "$0") [-q|--quiet] [-q|--no-prettify] [-f|--force]"
+            sleep 1
+            exit 1
+            ;;
+        esac
+        ;;
+    esac
+    shift
+  done
+fi
+
 git pull origin main
 
 # Or Install using symlink ?? not a very good idea
 function install_copy() {
-  rsync --exclude-from=.rsync_ignore -avhl --no-perms home/user/ ~
+  rsync -avh --exclude "home/user/git" home/user/ test/
+  [ -f "git_config.sh" ] && bash "git_config.sh" "${1}" "${2}"
   # shellcheck disable=SC1090
   source ~/.bash_profile
-  exec "$SHELL"
+  # exec "$SHELL"
 }
 
 function main() {
   if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
-    install_copy
+    shift
+    install_copy "${1}" "${2}"
   else
     __warning="This may overwrite existing files in your home directory. "
     __warning+="Are you sure? (y/n) "
     read -p "${__warning}" -rn 1
     echo ""
-    if [[ $REPLY =~ ^[Yy]$ ]]; then install_copy; fi
+    if [[ $REPLY =~ ^[Yy]$ ]]; then install_copy "${2}" "${3}"; fi
   fi
 }
 
-main "$@"
+main "${force}" "${verbose}" "${prettify}"
 unset install_copy
